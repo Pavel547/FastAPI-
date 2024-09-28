5937744f39dcde50c1c6ec4f0f771546d4a5bb01
Pavel547 <pawukok11@gmail.com>
2024-09-28 23:45:14 +0200

    New code with material

--- framework/Introduction_to_Fastapi.py
+++ framework/Introduction_to_Fastapi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -6,10 +7,76 @@ app = FastAPI()
 # async def root():
 #     return {"message :": "Hello world"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+# Path Parameters
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
 
 # @app.get("/congratulations/{name}")
 # async def congrat_user(name):
 #     return {"Congratulations you finishe the school": name}
+
+
+# class ModelName(str, Enum):
+#     alexnet = "alexnet"
+#     resnet = "resnet"
+#     lenet = "lenet"
+    
+# @app.get("/models/{model_name}")
+# async def get_model(model_name: ModelName):
+#     if model_name is ModelName.alexnet:
+#         return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+#     if model_name.value == "lenet":
+#         return {"model_name": model_name, "message": "LeCNN all the images"}
+
+#     return {"model_name": model_name, "message": "Have some residuals"}
+
+# Query Parameters
+
+# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+# @app.get("/items/")
+# async def red_item(skip: int=0, limit: int=5):
+#     return fake_items_db[skip : skip + limit]
+
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None):
+#     if q:
+#         return {"item_id": item_id, "q": q}
+#     return {"item_id": item_id}
+
+# Mini practies
+# @app.get("/users/{user_name}")
+# async def read_item(user_name: str, q: str | None = None, active: bool | None = None):
+#     user= {"user_name": user_name}
+    
+#     if active is True:
+#         user.update({"active": "user is active now"})
+#     if active is False:
+#         user.update({"active": "user in inactive now"})
+        
+#     return user
+
+# Query parameter type conversion
+
+# @app.get("/items/{item_id}")
+# async def read_iteme(item_id: str, q: str | None = None, short: bool = False):
+#     item = {"item_id": item_id}
+    
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update({"description": "This is an amazing item that has a long description"})
+#     return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
