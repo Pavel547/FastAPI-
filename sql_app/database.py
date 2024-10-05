16eaf009334bfb05bf5ca8a3f8d9a871c39d3125
Pavel547 <pawukok11@gmail.com>
2024-10-05 23:10:05 +0200

    Creat sql app add new file to my studying app and write code about models to db

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:sense9168@localhost/Fast_Api_study"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRESQL_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
