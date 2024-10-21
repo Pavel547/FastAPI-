942b8a907bfbab547bf39b297387bd5e8810b23f
Pavel547 <pawukok11@gmail.com>
2024-10-21 21:31:34 +0200

    I created the func for creater vechicle

--- sql_app_study/crud.py
+++ sql_app_study/crud.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id==user_id).first()
+    return db.query(models.User).filter(models.User.id == user_id).first()
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email==email).first()
+    return db.query(models.User).filter(models.User.email == email).first()
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
