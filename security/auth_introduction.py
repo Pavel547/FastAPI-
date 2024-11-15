8ebec6cffc7e1dd17c2949478d730b8dbc7fbc53
Pavel547 <pawukok11@gmail.com>
2024-11-15 23:27:23 +0100

    I finishd the final code

--- security/auth_introduction.py
+++ security/auth_introduction.py
@@ -157,7 +157,114 @@ fake_db = {
         "username": "user1",
         "full_name": "Jhon Doe",
         "email": "tester@test.com",
-        "hashed_passwoed": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
         "disabled": False,
     }
 }
+
+class Token(BaseModel):
+    access_token: str
+    type_token: str
+    
+class TokenData(BaseModel):
+    username: str | None = None
+    
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+    email: str | None = None
+    disabled: bool | None = None
+    
+class UserInDB(User):
+    hashed_password: str
+    
+pwd_context = CryptContext(schemes="bcrypt", deprecated="auto")
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+    
+def get_password_hash(password):
+    return pwd_context.hash(password)    
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+    
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expirec_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expirec_delta:
+        expire = datetime.now(timezone.utc) + expirec_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": "expire"})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=[ALGORITHM])
+    return encoded_jwt
+    
+async def get_current_user(token: Annotated[str, Depends(oauth2_schema)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+    
+@app.post("/token")
+async def login_for_acces_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+)->Token:
+    user = authenticate_user(fake_db, username=form_data.username, password=form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorect user name or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expirec_delta=access_token_expires
+    )
+    return Token(access_token=access_token, type_token="bearer")
+
+@app.get("/users/me", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
+
+@app.get("/users/me/items")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+        
