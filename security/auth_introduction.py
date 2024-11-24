c1bb33ba21cb27ec1bb66982515ec83df68ee5d2
Pavel547 <pawukok11@gmail.com>
2024-11-24 21:23:47 +0100

    I just practies

--- security/auth_introduction.py
+++ security/auth_introduction.py
@@ -182,15 +182,15 @@ app = FastAPI()
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
-
-def hashe_password(password):
+    
+def hashed_password(password):
     return pwd_context.hash(password)
 
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
-        return UserInDB(user_dict)
-    
+        return UserInDB(**user_dict)
+
 def authenticate_user(fake_db, username: str, password: str):
     user = get_user(fake_db, username)
     if not user: 
@@ -199,37 +199,67 @@ def authenticate_user(fake_db, username: str, password: str):
         return False
     return user
 
-def create_acces_token(data: dict, expire_time: timedelta | None = None):
+def create_access_token(data: dict, expire_time: timedelta | None = None):
     to_encode = data.copy()
     if expire_time:
         expire = datetime.now(timezone.utc) + expire_time
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+        
 async def get_current_user(token: Annotated[str, Depends(oauth2_sheme)]):
-    credentials_exception  = HTTPException(
+    credential_eception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid username or password",
-        headers={"WWW-Authenticate": "Bearer"}
+        headers={"WWW-Authenticete": "Bearer"}
     )
     try:
-        payload = jwt.encode(token, SECRET_KEY, algorithm=ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
         username: str = payload.get("sub")
         if username is None:
-            raise credentials_exception
+            return credential_eception
         token_data = TokenData(username=username)
     except InvalidTokenError:
-        raise credentials_exception 
-    user = get_user(fake_users_db, username=token_data.username)
+        raise credential_eception
+    user = get_user(fake_users_db, token_data.username)
     if user is None:
-        raise credentials_exception
+        raise credential_eception
     return user
-        
+
 async def get_current_active_user(
-  current_user: Annotated[User, Depends(get_current_user)]  
+    current_user: Annotated[User, Depends(get_current_user)]
 ):
-    if current_user is None:
-        raise HTTPException(status_code=400, detail="Inactive user")
+    if current_user.disabled:
+        return HTTPException(status_code=400, detail="Inactive user")
     return current_user
+
+@app.post("/token")
+async def login_for_acces_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    acces_token_expire = timedelta(minutes=ACCESS_TOKEM_EXPIRE_MINUTE)
+    acces_token = create_access_token(
+        data={"sub": user.username}, expires_delta=acces_token_expire
+    )
+    return Token(access_token=acces_token, token_type="bearer")
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
+
+@app.get("/users/me/items/")
+async def read_user_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
